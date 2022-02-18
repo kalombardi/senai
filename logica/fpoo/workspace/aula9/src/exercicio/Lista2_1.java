6b3f8e9ae11259b7130cbdc55b914a0a8b74e4be
kalombardi <kafa082009@gmail.com>
2022-02-18 11:20:05 -0300

    Att tarefa aula9

---
+++ logica/fpoo/workspace/aula9/src/exercicio/Lista2_1.java
@@ -0,0 +1,30 @@
+package exercicio;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lista2_1 {
+	public static Scanner entrada;
+	public static void main(String[] args) throws IOException{
+		entrada = new Scanner(System.in);
+		
+	}
+
+	public static void mostra() {
+		entrada = new Scanner(System.in);
+		
+		int[] indice = new int[5];
+		indice[0] = 2;
+		indice[1] = 4;
+		indice[2] = 5;
+		indice[3] = 6;
+		indice[4] = 8;
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println();
+
+			}
+
+	}
+	
+}
---
+++ logica/fpoo/workspace/aula9/src/exercicio/Lista2_2.java
@@ -0,0 +1,14 @@
+package exercicio;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lista2_2 {
+	public static Scanner entrada;
+	public static void main(String[] args) throws IOException{
+		entrada = new Scanner(System.in);
+		
+		
+	}
+
+}
