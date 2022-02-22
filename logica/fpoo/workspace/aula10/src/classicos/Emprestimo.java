323e5f439762a6f08e16f31645cdb2c3afca09c1
kalombardi <kafa082009@gmail.com>
2022-02-22 15:45:21 -0300

    Att

---
+++ logica/fpoo/workspace/aula10/src/classicos/Emprestimo.java
@@ -0,0 +1,14 @@
+package classicos;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Emprestimo {
+	public static Scanner entrada;
+	public static void main(String[] args) throws IOException{
+		entrada = new Scanner(System.in);
+		
+		
+	}
+
+}
